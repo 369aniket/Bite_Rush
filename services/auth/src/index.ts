f950bc56e4279619f4315a5b73f16ea4523d643a
Aniket Patel <aniketpatel494@gmail.com>
2026-08-11 22:14:37 +0530

    initializing the project

---
+++ services/auth/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import router from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Auth service is running on PORT ${PORT}`)
+    connectDB()
+})
---
+++ services/auth/src/model/User.model.ts
@@ -0,0 +1,34 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+export interface IUser extends Document{
+    name: string;
+    email: string;
+    image: string;
+    role: string;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: [true, 'Email address is required'],
+        unique: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+    },
+    role: {
+        type: String,
+        default: null,
+    }
+}, {timestamps: true})
+
+const User = mongoose.model<IUser>("User", userSchema)
+
+export default User;
