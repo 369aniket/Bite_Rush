908a282b838964ed27d9998049b8895eb7f18282
Aniket Patel <aniketpatel494@gmail.com>
2026-08-17 13:59:19 +0530

    feat(restaurant): implement restaurant service & add Mongoose restaurant schema

---
+++ services/restaurant/src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db.js";
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, ()=>{
+    console.log(`Restaurant service is running on PORT ${PORT}`)
+    connectDB()
+})
---
+++ services/restaurant/src/models/Restaurant.model.ts
@@ -0,0 +1,73 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface IRestaurant extends Document{
+    name: string;
+    description?: string;
+    image: string;
+    ownerId: string;
+    phone: number;
+    isVerified: boolean;
+
+    autoLocation: {
+        type: "Point",
+        coordinates:[number, number] // longitude latitude
+        formatedAddress: string;
+    };
+    isOpen: boolean;
+    createdAt: Date;
+}
+
+const RestaurantSchema = new Schema<IRestaurant>({
+    name:{
+        type:String,
+        required: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    ownerId:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: Number,
+        required:true,
+    },
+    isVerified:{
+        type: Boolean,
+        required: true,
+    },
+
+    autoLocation:{
+        type: {
+            type: String,
+            enum:["Point"],
+            required: true,
+        },
+        coordinates:{
+            type:[Number],
+            required:true,
+        },
+        formatedAddress:{
+            type:String,
+        }
+    },
+    isOpen:{
+        type: Boolean,
+        default: false,
+    }
+}, {timestamps: true})
+
+// To get index for nearby search
+
+RestaurantSchema.index({autoLocation:"2dsphere"})
+
+
+const Restaurant = mongoose.model<IRestaurant>("Restaurant", RestaurantSchema)
+
+export default Restaurant;
